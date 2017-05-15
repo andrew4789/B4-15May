e1f26092a3e1b8c57b0941e33879ef3b6c6c8f88
andrew4789 <10401121a@gmail.com>
2017-05-15 14:09:12 -0600

    1

--- src/GitHerDone.java
+++ src/GitHerDone.java
@@ -2,6 +2,8 @@
 public class GitHerDone {
 public static void main(String[] args){
 	System.out.println("initial build");
+	System.out.println("initial GUI");
+	System.out.println("initial build");
 }
 }
  
